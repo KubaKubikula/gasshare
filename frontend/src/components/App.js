3ce863bcb0f65767c2816e3d2e7a80319ac8e4d7
Kubakubikula <jakub.zient@gmail.com>
2021-09-28 19:55:52 +0100

    router module

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
 import logo from '../img/logo.svg';
 import '../css/App.css';
+import { GoogleLogin } from 'react-google-login';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 class App extends Component {
   render() {
+    const responseGoogle = (response) => {
+      console.log(response);
+    }
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">It's time to share your car/gasmoney</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/components/App.js</code> and save to reload.
-        </p>
+        <br /><br />
+        <GoogleLogin
+          clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+          buttonText="Login"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+          cookiePolicy={'single_host_origin'}
+        />
       </div>
     );
   }
