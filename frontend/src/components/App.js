735c22224d13e16df1a82b1d935c4382a2607335
Kubakubikula <jakub.zient@gmail.com>
2021-09-28 20:44:27 +0100

    form

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from '../img/logo.svg';
 import '../css/App.css';
 import { GoogleLogin } from 'react-google-login';
+
 import {
   BrowserRouter as Router,
   Switch,
@@ -15,12 +16,33 @@ class App extends Component {
       console.log(response);
     }
     return (
+      <Router>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">It's time to share your car/gasmoney</h1>
         </header>
         <br /><br />
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/driver">
+            <Driver />
+          </Route>
+          <Route path="/hitchhiker">
+            <Hitchhiker />
+          </Route>
+        </Switch>
+      </div>
+      </Router>
+    );
+    
+    function Login() {
+      return (
         <GoogleLogin
           clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
           buttonText="Login"
@@ -28,8 +50,55 @@ class App extends Component {
           onFailure={responseGoogle}
           cookiePolicy={'single_host_origin'}
         />
-      </div>
-    );
+      );
+    }
+
+    function Home() {
+      return (
+        <div>
+          <h2>I have</h2>
+          <div>
+          <Link to="/driver">
+            <input class="btn btn-primary" type="button" value="Car" />
+          </Link>
+          <br /><br />
+          <Link to="/hitchhiker">
+            <input class="btn btn-primary" type="button" value="Gas Money" />
+          </Link>
+          </div>
+        </div>
+      );
+    }
+    
+    function Driver() {
+      return ( 
+        <div>
+        <h2>Where are you going ?</h2>
+          <input type="text" value="From address" />
+          <br />
+          <input type="text" value="Address" />
+          <br />
+          <input type="text" value="Time" />
+          <br />
+          <input class="btn btn-primary" type="button" value="Submit" />
+        </div>
+      );
+    }
+
+    function Hitchhiker() {
+      return ( 
+        <div>
+        <h2>Where are you going ?</h2>
+          <input type="text" value="From address" />
+          <br />
+          <input type="text" value="Address" />
+          <br />
+          <input type="text" value="Time" />
+          <br />
+          <input class="btn btn-primary" type="button" value="Submit" />
+        </div>
+      );
+    }
   }
 }
 
