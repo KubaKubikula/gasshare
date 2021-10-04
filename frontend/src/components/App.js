5ef35a769541ef2673dbef60b0db855a037cf0ec
Kubakubikula <jakub.zient@gmail.com>
2021-10-04 18:37:40 +0100

    django python react fun

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from '../img/logo.svg';
+import logo from '../img/logo.png';
 import '../css/App.css';
 import { GoogleLogin } from 'react-google-login';
 import {
@@ -15,12 +15,12 @@ class App extends Component {
       console.log(response);
     }
     return (
-      <Router>
+      <Router>  
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">It's time to share your car/gasmoney</h1>
-        </header>
+          <header className="App-header">
+            <img className="App-logo" src={logo} alt="logo" />
+            <h1 className="App-title">It's time to share your car/gasmoney</h1>
+          </header>
         <br /><br />
         <Switch>
           <Route path="/login">
@@ -34,6 +34,9 @@ class App extends Component {
           </Route>
           <Route path="/hitchhiker">
             <Hitchhiker />
+          </Route>
+          <Route path="/drives">
+            <Drives />
           </Route>
         </Switch>
       </div>
@@ -73,13 +76,7 @@ class App extends Component {
       return ( 
         <div>
         <h2>Where are you going ?</h2>
-          <input type="text" value="From address" />
-          <br />
-          <input type="text" value="Address" />
-          <br />
-          <input type="text" value="Time" />
-          <br />
-          <input class="btn btn-primary" type="button" value="Submit" />
+          <DriverForm />
         </div>
       );
     }
@@ -98,6 +95,89 @@ class App extends Component {
         </div>
       );
     }
+
+    function Drives() {
+      return ( 
+        <div>
+        <h2>Drives</h2>
+          <table>
+            <tr>
+              <td>Lisabon - Porto</td>
+              <td>10am</td>
+              <td>Knight rider</td>
+            </tr>
+          </table>
+        </div>
+      );
+    }
+  }
+}
+
+class DriverForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {from: '', to: '', when: ''};
+    
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    if (event.target.name === "from") {
+      this.setState({from: event.target.value});
+    }
+
+    if (event.target.name === "to") {
+      this.setState({to: event.target.value});
+    }
+
+    if (event.target.name === "when") {
+      this.setState({when: event.target.value});
+    }
+  }
+
+  handleSubmit(event) {
+    
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div>
+      <form onSubmit={this.handleSubmit}>
+        <table style={{display: 'block', width: '100px', margin: '0px auto'}}>
+        <tbody>
+          <tr>
+            <td>
+            From:
+            </td>
+            <td>
+              <input type="text" name="from" value={this.state.from} onChange={this.handleChange} /> Insert my location
+            </td>
+          </tr>
+          <tr>
+            <td>
+            To:
+            </td>
+            <td>
+              <input type="text" name="to" value={this.state.to} onChange={this.handleChange} />
+            </td>
+          </tr>
+          <tr>
+            <td>
+            When:
+            </td>
+            <td>
+              <input type="text" name="when" value={this.state.when} onChange={this.handleChange} />
+            </td>
+          </tr>
+          </tbody>
+        </table>
+        <input type="submit" value="Submit" />
+      </form>
+      </div>
+    );
   }
 }
 
